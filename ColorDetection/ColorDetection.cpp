c273d3ef565494d6613304f3eacd88f94c8f59c3
luptiwahyu <luptiwahyu@ymail.com>
2013-01-12 13:29:23 +0700

    Tugas Sismul

---
+++ ColorDetection/ColorDetection.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+//#include <opencv/cxcore.h>
+
+IplImage* GetGambarThreshold(IplImage* gbrHSV){
+	IplImage* gbrThresh=cvCreateImage(cvGetSize(gbrHSV),IPL_DEPTH_8U,1);
+	cvInRangeS(gbrHSV,cvScalar(170,160,60),cvScalar(180,256,256),gbrThresh);
+	return gbrThresh;
+}
+
+int main(){
+	CvCapture* gambar = cvCaptureFromFile("3d-09-5balls.jpg");
+	
+	IplImage* frame=0;
+
+	cvNamedWindow("input", CV_WINDOW_AUTOSIZE);
+	cvNamedWindow("output", CV_WINDOW_AUTOSIZE);
+
+	while(true){
+
+		frame = cvQueryFrame(gambar);
+		if(!frame) 
+			break;
+
+		frame = cvCloneImage(frame);
+		cvSmooth(frame, frame, CV_GAUSSIAN,3,3);
+
+		IplImage* gbrHSV = cvCreateImage(cvGetSize(frame),IPL_DEPTH_8U,3);
+		cvCvtColor(frame,gbrHSV,CV_BGR2HSV); 
+		IplImage* gbrThresh = GetGambarThreshold(gbrHSV);
+
+		cvSmooth(gbrThresh,gbrThresh,CV_GAUSSIAN,3,3);
+
+		cvShowImage("output",gbrThresh);
+		cvShowImage("input",frame);
+
+		cvReleaseImage(&gbrHSV);
+		cvReleaseImage(&gbrThresh);
+		cvReleaseImage(&frame);
+		
+		cvWaitKey();
+	}
+
+	cvDestroyAllWindows();	
+	cvReleaseCapture(&gambar);
+
+	return 0;
+}
+
+
