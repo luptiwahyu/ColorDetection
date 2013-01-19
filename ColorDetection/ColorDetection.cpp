170616e10812d3d0b07d2678471a605539226a8c
Lupti Wahyu <luptiwahyu@ymail.com>
2013-01-19 07:52:30 +0700

    update tracking red object (final)

--- ColorDetection/ColorDetection.cpp
+++ ColorDetection/ColorDetection.cpp
@@ -1,12 +1,48 @@
 #include "stdafx.h"
 #include <opencv/cv.h>
 #include <opencv/highgui.h>
+#include <ctime>
+
+using namespace std;
+
+clock_t start;	
+	
+IplImage* imgTracking;	
+int lastX = -1;	
+int lastY = -1;	
 
 // mengubah gambar menjadi threshold
 IplImage* GetGambarThreshold(IplImage* gbrHSV){
 	IplImage* gbrThresh=cvCreateImage(cvGetSize(gbrHSV),IPL_DEPTH_8U,1);
 	cvInRangeS(gbrHSV,cvScalar(170,160,60),cvScalar(180,256,256),gbrThresh); //scalar untuk menunjukan nilai atas dan nilai bawah
 	return gbrThresh;
+}
+
+void trackObject(IplImage* gbrThresh){	
+	// calculate the moments of 'gbrThresh'
+
+	CvMoments *moments = (CvMoments*)malloc(sizeof(CvMoments));	
+	cvMoments(gbrThresh, moments, 1);	
+	double moment10 = cvGetSpatialMoment(moments,1,0);	
+	double moment01 = cvGetSpatialMoment(moments,0,1);	
+	double area = cvGetCentralMoment(moments,0,0);	
+
+	// if the area <1000, i consider that the there are no object in the image and it's because of the noise, the area is not zero
+	if(area>1000){	
+		// calculate the position of the ball
+		int posX = moment10/area;	
+		int posY = moment01/area;	
+
+		if(lastX>=0 && lastY>=0 && posX>=0 && posY>=0){	
+			// draw a red line from the previous point to the current point
+			cvLine(imgTracking,cvPoint(posX,posY),cvPoint(lastX,lastY),cvScalar(0,0,255),4);	
+		}	
+
+		lastX = posX;	
+		lastY = posY;	
+	}	
+
+	free(moments);	
 }
 
 int main(){
@@ -20,11 +56,21 @@ int main(){
 	}
 
 	IplImage* frame=0;
+	
+	int i=1;
+	
+	frame = cvQueryFrame(kamera);	
+	if(!frame) return -1;	
+	// create a blank image and assigned to 'imgTracking' which has the same size of original video 
+	imgTracking = cvCreateImage(cvGetSize(frame),IPL_DEPTH_8U,3);	
+	cvZero(imgTracking); // covert the image, 'imgTracking' to black
 
 	cvNamedWindow("input", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("output", CV_WINDOW_AUTOSIZE);
 
-	while(true){
+	while(1){
+		
+		start = clock();
 
 		frame = cvQueryFrame(kamera);
 		if(!frame) 
@@ -38,6 +84,12 @@ int main(){
 		IplImage* gbrThresh = GetGambarThreshold(gbrHSV);
 
 		cvSmooth(gbrThresh,gbrThresh,CV_GAUSSIAN,3,3);
+		
+		// track the position of the ball 
+		trackObject(gbrThresh);	
+
+		// add the tracking image and the frame
+		cvAdd(frame,imgTracking,frame);	
 
 		cvShowImage("output",gbrThresh);
 		cvShowImage("input",frame);
@@ -45,6 +97,8 @@ int main(){
 		cvReleaseImage(&gbrHSV);
 		cvReleaseImage(&gbrThresh);
 		cvReleaseImage(&frame);
+		
+		cout << i++ << " " << ((clock()-start) / 16)<< endl;
 		
 		int c = cvWaitKey(10);
 
@@ -54,6 +108,7 @@ int main(){
 
 	cvDestroyAllWindows();	
 	cvReleaseCapture(&kamera);
+	cvReleaseImage(&imgTracking);	
 
 	return 0;
 }
